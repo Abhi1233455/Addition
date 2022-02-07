4ea3a2a142745326edbaf5894094068c998c9967
Abhishek <abhishekgond634@gmail.com>
2022-02-07 19:23:52 +0530

    Commit add

---
+++ src/Add.java
@@ -0,0 +1,10 @@
+public class Add {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 23;
+        int c = 44;
+        int d = 55;
+        int sum = (a+b+c+d);
+        System.out.println(sum);
+    }
+}
